614432e09cb49f682e28ea5e4e007e4e73e67118
dineshchary <49143877+dineshchary@users.noreply.github.com>
2019-04-03 16:39:36 +0530

    Update App.java

--- src/main/java/GIT/commit/App.java
+++ src/main/java/GIT/commit/App.java
@@ -15,7 +15,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
         
-        String name="AAAASDFG"
+       
         
         
     }
